f828367c2e81699efc9a685d43a7598170d94c11
griffinwt <69866169+griffinwt@users.noreply.github.com>
2021-03-20 10:53:15 -0600

    debug

--- Hackathon_Streamlit2.py
+++ Hackathon_Streamlit2.py
@@ -4,10 +4,10 @@ import pandas as pd
 #REMINDER - to preview in local browser: strealit run file_name.py (in bash)
 
 #custom imports
--m pip install matplotlib
--m pip install sklearn.model_selection
--m pip install statsmodels
--m pip install sklearn.metrics
+py -m pip install matplotlib
+py -m pip install sklearn.model_selection
+py -m pip install statsmodels
+py -m pip install sklearn.metrics
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
